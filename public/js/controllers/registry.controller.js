1300dcb8895ff96c6f1ce0fdb4af30143135a086
Erin McCaffrey <erinmccaffrey@Erins-MacBook-Pro.local>
2017-05-04 17:25:58 -0400

    fixed reload issue for registry script

---
+++ public/js/controllers/registry.controller.js
@@ -0,0 +1,14 @@
+(function() {
+  angular.module('erinanddustin')
+          .controller('RegistryController', RegistryController);
+
+  RegistryController.$inject = ['$scope', '$window', '$location', '$route'];
+
+  function RegistryController($scope, $window, $location, $route){
+
+    $scope.reloadRoute = function () {
+      $window.location.reload();
+    }
+  }
+
+}());
